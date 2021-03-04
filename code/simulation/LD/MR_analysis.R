5601cbda137fbcdd078de0b96c9c0c9c1255f1a1
Zhang <andrew.haoyu@gmail.com>
2021-03-04 12:35:39 -0500

    MR_analysis

--- code/simulation/LD/MR_analysis.R
+++ code/simulation/LD/MR_analysis.R
@@ -1,86 +1,125 @@
 args = commandArgs(trailingOnly = T)
 l = as.numeric(args[[1]])
+sub = as.numeric(args[[2]])
 library(withr)
 #with_libpaths(new = "/home/zhangh24/R/x86_64-pc-linux-gnu-library/3.6/", install_github('qingyuanzhao/mr.raps'))
 #install_github('qingyuanzhao/mr.raps')
 library(mr.raps)
 library(MendelianRandomization)
 library(MRPRESSO)
+library(data.table)
+library(dplyr)
 setwd("/data/zhangh24/MR_MA/")
 source("./code/simulation/functions/MR_function.R")
+cur.dir <- "/data/zhangh24/MR_MA/result/LD/"
+j =22
 sum.data.y = as.data.frame(fread(paste0(cur.dir,"y_summary_chr_",j,"_rho_",l)))
 sum.data.m = as.data.frame(fread(paste0(cur.dir,"m_summary_chr_",j,"_rho_",l)))
 n.snp = nrow(sum.data.m)
 n.rep = 100
-for(i_rep in  1:n.rep){
+
+num = 10
+library(bc2)
+start.end = startend(n.rep,num,sub)
+start = start.end[1]
+end = start.end[2]
+total = (end-start+1) *6
+MR_result <- matrix(NA,total,4)
+
+colnames(MR_result) <- c("est","sd","cover","method")
+temp = 1
+beta_M = 0.15
+for(i_rep in  start:end){
   p = sum.data.m[,(6+3*i_rep)]
-  print(range(p))
-}
   idx = which(p<=0.05/n.snp)
-  Gamma = sum.data.y[idx,(6+3*i_rep-2)]
-  var_Gamma = (sum.data.y[idx,(6+3*i_rep-2)]/sum.data.y[idx,(6+3*i_rep-1)])^2
-  gamma = sum.data.m[idx,(6+3*i_rep-2)]
-  var_gamma = (sum.data.m[idx,(6+3*i_rep-2)]/sum.data.m[idx,(6+3*i_rep-1)])^2
-  
-  num = 3
-  IVW_c_temp <- IVW_c(Gamma,var_Gamma,
-                      gamma,var_gamma)
-  IVW_c_result[k] <- paste0(round(IVW_c_temp[[1]],num)," (",round(IVW_c_temp[[4]],num),")")
-  MR_weight_temp = MRWeight(Gamma,var_Gamma,
-                            gamma,var_gamma)
-  keep.id = MR_weight_temp[[5]]
-  out.id = MR_weight_temp[[6]]
-  
-  keep.ind = rep(FALSE,length(Gamma))
-  keep.ind[out.id] = TRUE
-  keep.ind = factor(keep.ind,levels=c(TRUE,FALSE))
-  lm(Gamma[keep.id]~gamma[keep.id])
-  
-  beta_est = MR_weight_temp[[1]]
-  plot.data = data.frame(gamma,Gamma,Weight = 1/(var_Gamma+beta_est^2*var_gamma),Removed = keep.ind)
-  p = ggplot(plot.data,aes(gamma,Gamma))+
-    geom_point(aes(col=Removed,size = Weight),alpha = 0.9)+
-    geom_abline(slope = MR_weight_temp[[1]],intercept = 0)+
-    theme_Publication()+
-    scale_colour_Publication()+
-    ggtitle(paste0(trait_vec[k]))+
-    xlab("Alpha")+
-    geom_abline(slope =-0.502,intercept = 0.006, linetype = "dashed")
-  
-  png(filename = paste0("./result/real_data_analysis/",trait_vec[k],"annotate.png"),width = 8,height = 6, units = "in",res= 300)
-  print(p)
-  dev.off()
-  
-  MRweight_result[k] <- paste0(round(MR_weight_temp[[1]],num)," (",round(MR_weight_temp[[4]],num),")")
-  MRInputObject <- mr_input(bx = gamma,
-                            bxse = sqrt(var_gamma),
-                            by = Gamma,
-                            byse = sqrt(var_Gamma))
-  median_result <- mr_median(MRInputObject,
-                             weighting = "weighted",
+  if(length(idx)!=0){
+    Gamma = sum.data.y[idx,(6+3*i_rep-2)]
+    var_Gamma = (sum.data.y[idx,(6+3*i_rep-2)]/sum.data.y[idx,(6+3*i_rep-1)])^2
+    gamma = sum.data.m[idx,(6+3*i_rep-2)]
+    var_gamma = (sum.data.m[idx,(6+3*i_rep-2)]/sum.data.m[idx,(6+3*i_rep-1)])^2
+    
+    num = 3
+    IVW_c_temp <- IVW_c(Gamma,var_Gamma,
+                        gamma,var_gamma)
+    MR_result[temp,1] = c(IVW_c_temp[1])
+    MR_result[temp,2] = c(IVW_c_temp[4])
+    MR_result[temp,3] = ifelse(IVW_c_temp[2]<=beta_M&
+                            IVW_c_temp[3]>=beta_M,1,0)
+    MR_result[temp,4] = "IVW"
+    temp   = temp + 1
+    MR_weight_temp = MRWeight(Gamma,var_Gamma,
+                              gamma,var_gamma)
+    MR_result[temp,1] = as.numeric(MR_weight_temp[[1]][1])
+    MR_result[temp,2] = c(MR_weight_temp[[4]][1])
+    MR_result[temp,3] = ifelse(MR_weight_temp[[2]][1]<=beta_M&
+                                 MR_weight_temp[[3]][1]>=beta_M,1,0)
+    MR_result[temp,4] = "MR-Weight"
+    temp = temp+1
+    MRInputObject <- mr_input(bx = gamma,
+                              bxse = sqrt(var_gamma),
+                              by = Gamma,
+                              byse = sqrt(var_Gamma))
+    median_result <- mr_median(MRInputObject,
+                               weighting = "weighted",
+                               distribution = "normal",
+                               alpha = 0.05,
+                               iterations = 10000,
+                               seed = 314159265)
+    MR_result[temp,1] = median_result$Estimate
+    MR_result[temp,2] = median_result$StdError
+    MR_result[temp,3] = ifelse(median_result$CILower<=beta_M&
+                                 median_result$CIUpper>=beta_M,1,0)
+    MR_result[temp,4] = "MR-Median"
+    temp = temp +1
+    egger_result <- mr_egger(MRInputObject,
+                             robust = FALSE,
+                             penalized = FALSE,
+                             correl = FALSE,
                              distribution = "normal",
-                             alpha = 0.05,
-                             iterations = 10000,
-                             seed = 314159265)
-  MR_median_result[k] <- paste0(round(median_result$Estimate,num)," (",round(median_result$StdError,num),")")
-  egger_result <- mr_egger(MRInputObject,
-                           robust = FALSE,
-                           penalized = FALSE,
-                           correl = FALSE,
-                           distribution = "normal",
-                           alpha = 0.05)
-  MR_egger_result[k] <- paste0(round(egger_result$Estimate,num)," (",round(egger_result$StdError.Est,num),")")
-  raps_result <- mr.raps(data = data.frame(beta.exposure = gamma,
-                                           beta.outcome = Gamma,
-                                           se.exposure = sqrt(var_gamma),
-                                           se.outcome = sqrt(var_Gamma)),
-  )
-  MR_raps_result[k] <- paste0(round(raps_result$beta.hat,num)," (",round(raps_result$beta.se,num),")")
-  summary.data = data.frame(E1_effect = gamma,
-                            E1_se = sqrt(var_gamma),
-                            
-                            Y_effect = Gamma,
-                            Y_se = sqrt(var_Gamma))
-  presso_result <- mr_presso(BetaOutcome = "Y_effect", BetaExposure = "E1_effect", SdOutcome = "Y_se", SdExposure = "E1_se", OUTLIERtest = TRUE, DISTORTIONtest = TRUE, data = summary.data, NbDistribution = 1000,  SignifThreshold = 0.05)
-  mr_presso_result[k] = paste0(round(presso_result$`Main MR results`[1,3],num)," (",round(presso_result$`Main MR results`[1,4],num),")")
+                             alpha = 0.05)
+    MR_result[temp,1] = egger_result$Estimate
+    MR_result[temp,2] = egger_result$StdError.Est
+    MR_result[temp,3] = ifelse(egger_result$CILower.Est<=beta_M&
+                                 egger_result$CIUpper.Est>=beta_M,1,0)
+    MR_result[temp,4] = "MR-Egger"
+    temp = temp +1
+    raps_result <- mr.raps(data = data.frame(beta.exposure = gamma,
+                                             beta.outcome = Gamma,
+                                             se.exposure = sqrt(var_gamma),
+                                             se.outcome = sqrt(var_Gamma)),
+    diagnostics = F)
+    MR_result[temp,1] = raps_result$beta.hat
+    MR_result[temp,2] = raps_result$beta.se
+    MR_result[temp,3] = ifelse(raps_result$beta.hat-1.96*raps_result$beta.se<=beta_M&
+                                 raps_result$beta.hat+1.96*raps_result$beta.se>=beta_M,1,0)
+    MR_result[temp,4] = "Raps"
+    temp = temp+1
+    summary.data = data.frame(E1_effect = gamma,
+                              E1_se = sqrt(var_gamma),
+                              
+                              Y_effect = Gamma,
+                              Y_se = sqrt(var_Gamma))
+    presso_result <- mr_presso(BetaOutcome = "Y_effect", BetaExposure = "E1_effect", SdOutcome = "Y_se", SdExposure = "E1_se", OUTLIERtest = TRUE, DISTORTIONtest = TRUE, data = summary.data, NbDistribution = 1000,  SignifThreshold = 0.05)
+    MR_result[temp,1] = presso_est = presso_result$`Main MR results`[1,3]
+    MR_result[temp,2] = presso_sd = presso_result$`Main MR results`[1,4]
+    MR_result[temp,3] = ifelse(presso_est-1.96*presso_sd<=beta_M&
+                                 presso_est+1.96*presso_sd>=beta_M,1,0)
+    MR_result[temp,4] = "MR-PRESSO"
+    
+  }else{
+    MR_result[temp,4] = "IVW"
+    temp = temp+1
+    MR_result[temp,4] = "MR-Weight" 
+    temp  = temp+1
+    MR_result[temp,4] = "MR-Median"
+    temp = temp+1
+    MR_result[temp,4] = "MR-Egger"
+    temp = temp+1
+    MR_result[temp,4] = "Raps"
+    temp = temp+1
+    MR_result[temp,4] = "MR-PRESSO"
+    temp = temp+1
+    
+  }
 }
+save(MR_result,file =paste0(cur.dir,"MR_result_rho_",l,"_sub_",sub,".rdata"))

db06c816326dc3cb04f50ecb10229c9ad0ffe607
Zhang <andrew.haoyu@gmail.com>
2021-03-04 01:13:35 -0500

    update

--- code/simulation/LD/run_gwas.R
+++ code/simulation/LD/run_gwas.R
@@ -1,9 +1,16 @@
 
 l = as.numeric(args[[1]])
+#i1 represent the sub id, split 100 replciates into 10
+i1 = as.numeric(args[[2]])
 j = 22
-
+num = 10
 library(dplyr)
-
+library(bc2)
+library(data.table)
+n_rep = 100
+start.end = startend(n.rep,num,i1)
+start = start.end[1]
+end = start.end[2]
 #load the phenotpypes data and use plink to run
   cur.dir <- "/data/zhangh24/MR_MA/result/LD/"
 
@@ -14,7 +21,11 @@ library(dplyr)
   system(paste0("cp ", cur.dir,"chr",j,".hm3.bim /lscratch/",sid,"/test/chr",j,".hm3.bim"))
   system(paste0("cp ", cur.dir,"chr",j,".hm3.fam /lscratch/",sid,"/test/chr",j,".hm3.fam"))
   
-  res <- system(paste0("/data/zhangh24/software/plink2 --threads 2 --bfile /lscratch/",sid,"/test/chr",j,".hm3 --out ",temp.dir,"y_summary_chr_",j,"_rho_",l,".out --linear --all-pheno --allow-no-sex --pheno ",cur.dir,"y_pheno_plink_rho_",l,".phen"))
+  pheno_y = as.data.frame(fread(paste0(cur.dir,"y_pheno_plink_rho_",l,".phen")))
+  
+  pheno_y_sub = pheno_y[,c(1:2,(2+start):(2+end))]
+  write.table(pheno_y_sub,file = paste0(temp.dir,"pheno_y_sub_",i1,".phen"),row.names = F,col.names =F,quote=F )
+  res <- system(paste0("/data/zhangh24/software/plink2 --threads 2 --bfile /lscratch/",sid,"/test/chr",j,".hm3 --out ",temp.dir,"y_summary_chr_",j,"_rho_",l,".out --linear --all-pheno --allow-no-sex --pheno ",temp.dir,"pheno_y_sub_",i1,".phen"))
   if(res==2){
     stop()
   }
@@ -22,16 +33,20 @@ library(dplyr)
   sum.data = fread(paste0(temp.dir,"y_summary_chr_",j,"_rho_",l,".out.P",i_rep,".assoc.linear"))
   sum.data.infor = sum.data[,1:6]
   sum.data.list = list()
-  for(i_rep in 1:3){
+  for(i_rep in 1:(end-start+1)){
     sum.data = fread(paste0(temp.dir,"y_summary_chr_",j,"_rho_",l,".out.P",i_rep,".assoc.linear"))
     effect = sum.data[,c(7:9)]
     sum.data.list[[i_rep]] = effect
   }
   effect = bind_cols(sum.data.list)
   sum.data = cbind(sum.data.infor,effect)
-  write.table(sum.data,file = paste0(cur.dir,"y_summary_chr_",j,"_rho_",l),row.names = F,col.names = T,quote=F)
+  write.table(sum.data,file = paste0(cur.dir,"y_summary_chr_",j,"_rho_",l,"_sub_",i1),row.names = F,col.names = T,quote=F)
   
-  system(paste0('rm -rf /lscratch/',sid,'/test/y_summary_chr_*'))
+  #system(paste0('rm -rf /lscratch/',sid,'/test/y_summary_chr_*'))
+  
+  pheno_m= as.data.frame(fread(paste0(cur.dir,"m_pheno_plink_rho_",l,".phen")))
+  pheno_m_sub = pheno_m[,c(1:2,(2+start):(2+end))]
+  write.table(pheno_m_sub,file = paste0(temp.dir,"pheno_m_sub_",i1,".phen"),row.names = F,col.names =F,quote=F )
   
   res <- system(paste0("/data/zhangh24/software/plink2 --threads 2 --bfile /lscratch/",sid,"/test/chr",j,".hm3 --out ",temp.dir,"m_summary_chr_",j,"_rho_",l,".out --linear --all-pheno --allow-no-sex --pheno ",cur.dir,"y_pheno_plink_rho_",l,".phen"))
   if(res==2){
@@ -41,16 +56,16 @@ library(dplyr)
   sum.data = fread(paste0(temp.dir,"m_summary_chr_",j,"_rho_",l,".out.P",i_rep,".assoc.linear"))
   sum.data.infor = sum.data[,1:6]
   sum.data.list = list()
-  for(i_rep in 1:3){
+  for(i_rep in 1:(end-start+1)){
     sum.data = fread(paste0(temp.dir,"m_summary_chr_",j,"_rho_",l,".out.P",i_rep,".assoc.linear"))
     effect = sum.data[,c(7:9)]
     sum.data.list[[i_rep]] = effect
   }
   effect = bind_cols(sum.data.list)
   sum.data = cbind(sum.data.infor,effect)
-  write.table(sum.data,file = paste0(cur.dir,"m_summary_chr_",j,"_rho_",l),row.names = F,col.names = T,quote=F)
+  write.table(sum.data,file = paste0(cur.dir,"m_summary_chr_",j,"_rho_",l,"_sub_",i1),row.names = F,col.names = T,quote=F)
   
-  system(paste0('rm -rf /lscratch/',sid,'/test/m_summary_chr_*'))
+  
   
 
 # for(i in 1:5){
